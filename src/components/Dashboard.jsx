c3d219cd7c516a0d1d9ab10f8ce5e6ffdd9acec3
victor <victorifeanyichukwu4@gmail.com>
2022-09-22 11:56:30 +0100

    made sidebar responsive

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -11,6 +11,7 @@ import { useAPI } from "../components/Apicontext";
 function Dashboard() {
     const navigate = useNavigate();
     const { filmData, loading, error } = useAPI();
+    console.log(filmData[4].title);
 
     const handleRowClick = (films) => {
         navigate(`/dashboard/${films.title}`);
@@ -19,53 +20,56 @@ function Dashboard() {
     if (error) return <h1>Error!</h1>;
 
     return (
-        <div className="container-fluid">
-            <div className="row">
-                <Navbar />
+        <>
 
-                <div className="col p-0">
-                    <Topnav />
-                    <Cardcontainer />
+            <div className="container-fluid">
+                <div className="row">
+                    <Navbar />
 
-                    <div className="container mb-5">
-                        <p className="film">Films</p>
-                        <div className="responsive">
-                            <table>
-                                <tr>
-                                    <th><i className="fa-solid fa-square"></i></th>
-                                    <th>Film title</th>
-                                    <th>Released Date</th>
-                                    <th>Director</th>
-                                    <th>Producer</th>
-                                    <th>Episode ID</th>
-                                    <th>Character</th>
-                                </tr>
-                                {loading ? (
-                                    <h1>Loading...</h1>
-                                ) : (filmData.map((film) =>
-                                    <tr className="pointer" onClick={() => handleRowClick(film)}>
-                                        <Tabledata
-                                            rectangle={<i className=" fa-solid fa-square"></i>}
-                                            firstcolumn={film.title}
-                                            secondcolumn={film.release_date}
-                                            thirdcolumn={film.director}
-                                            fouthcolumn={film.producer}
-                                            fifthcolumn={film.episode_id}
-                                            sixthcolumn="https://swapi.dev/api/people"
-                                        />
+                    <div className="col p-0">
+                        <Topnav />
+                        <Cardcontainer />
+
+                        <div className="container mb-5">
+                            <p className="film">Films</p>
+                            <div className="responsive">
+                                <table>
+                                    <tr>
+                                        <th><i className="fa-solid fa-square"></i></th>
+                                        <th>Film title</th>
+                                        <th>Released Date</th>
+                                        <th>Director</th>
+                                        <th>Producer</th>
+                                        <th>Episode ID</th>
+                                        <th>Character</th>
                                     </tr>
-                                ))}
-                            </table>
+                                    {loading ? (
+                                        <h1>Loading...</h1>
+                                    ) : (filmData.map((film) =>
+                                        <tr className="pointer" onClick={() => handleRowClick(film)}>
+                                            <Tabledata
+                                                rectangle={<i className=" fa-solid fa-square"></i>}
+                                                firstcolumn={film.title}
+                                                secondcolumn={film.release_date}
+                                                thirdcolumn={film.director}
+                                                fouthcolumn={film.producer}
+                                                fifthcolumn={film.episode_id}
+                                                sixthcolumn="https://swapi.dev/api/people"
+                                            />
+                                        </tr>
+                                    ))}
+                                </table>
 
+                            </div>
                         </div>
+
+
+
                     </div>
-
-
-
                 </div>
-            </div>
 
-        </div>
+            </div>
+        </>
     );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,40 +6,49 @@ function Navbar() {
 
     return (
 
-        <div className="col-auto min-vh-100 bg-color">
+        <nav className="navbar navbar-expand-lg navbar-light bg-color">
+            <div className="container-fluid">
+                {/* <a className="navbar-brand" href="#">Navbar</a> */}
+                <img className="logo mt-5 mb-4" src="/img/logo.png" alt="logo" />
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    {/* <span className="navbar-toggler-icon"></span> */}
+                    <i class="fa-solid fa-bars"></i>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="navbar-nav">
 
-            <img className="logo mt-5 mb-4" src="/img/logo.png" alt="logo" />
+                        <NavLink className={({ isActive }) =>
+                            isActive ? 'nav-link bg-blue text-white px-4 w-100' : 'nav-link text-white px-4 w-100'
+                        } to="/dashboard">
+                            <i className="fa-regular fa-window-restore"></i>&nbsp; &nbsp; Overview
+                        </NavLink>
 
-            <NavLink className={({ isActive }) =>
-                isActive ? 'nav-link bg-blue text-white px-4 w-100' : 'nav-link text-white px-4 w-100'
-            } to="/dashboard">
-                <i className="fa-regular fa-window-restore"></i><span className="ms-1 d-none d-sm-inline">&nbsp; &nbsp; Overview</span>
-            </NavLink>
+                        <div className="mt-5">
 
-            <div className="mt-5">
+                            <NavLink className={({ isActive }) =>
+                                isActive ? 'nav-link bg-blue text-white px-4 w-100' : 'nav-link text-white px-4 w-100'
+                            } to="/starships">
+                                <i className="fa-solid fa-square bg-sb"></i>&nbsp; &nbsp; Starships
+                            </NavLink>
 
-                <NavLink className={({ isActive }) =>
-                    isActive ? 'nav-link bg-blue text-white px-4 w-100' : 'nav-link text-white px-4 w-100'
-                } to="/starships">
-                    <i className="fa-solid fa-square bg-sb"></i><span className="ms-1 d-none d-sm-inline">&nbsp; &nbsp; Starships</span>
-                </NavLink>
+                            <NavLink className={({ isActive }) =>
+                                isActive ? 'nav-link bg-blue text-white px-4 w-100' : 'nav-link text-white px-4 w-100'
+                            } to="/people">
+                                <i className="fa-solid fa-square bg-lp"></i>&nbsp; &nbsp; People
+                            </NavLink>
 
-                <NavLink className={({ isActive }) =>
-                    isActive ? 'nav-link bg-blue text-white px-4 w-100' : 'nav-link text-white px-4 w-100'
-                } to="/people">
-                    <i className="fa-solid fa-square bg-lp"></i><span className="ms-1 d-none d-sm-inline">&nbsp; &nbsp; People</span>
-                </NavLink>
+                            <NavLink className={({ isActive }) =>
+                                isActive ? 'nav-link bg-blue text-white px-4 w-100' : 'nav-link text-white px-4 w-100'
+                            } to="/species">
+                                <i className="fa-solid fa-square bg-sy"></i>&nbsp; &nbsp; Species
+                            </NavLink>
+                        </div>
 
-                <NavLink className={({ isActive }) =>
-                    isActive ? 'nav-link bg-blue text-white px-4 w-100' : 'nav-link text-white px-4 w-100'
-                } to="/species">
-                    <i className="fa-solid fa-square bg-sy"></i><span className="ms-1 d-none d-sm-inline">&nbsp; &nbsp; Species</span>
-                </NavLink>
+
+                    </div>
+                </div>
             </div>
-
-        </div>
-
-
+        </nav>
 
     );
 }
